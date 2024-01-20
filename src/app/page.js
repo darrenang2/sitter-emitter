6a238cfd0ba98e2ba6a1e7b558ce43641a58d828
darrenang2 <darrenang2001@gmail.com>
2024-01-20 15:52:58 -0800

    Updated textboxes and changed background

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,18 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Thorough background check for babysitters (submitter review system to ensure exceptional service)
-        </p>
-        <p className="fixed right-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Babysitters are sent to houses when they are needed
-        </p>
+        <div className="flex justify-center space-x-4 mx-8">
+          <p className="flex flex-1 justify-center bg-gray-400 rounded p-6 shadow">
+            Thorough background check and certification to ensure well trained
+            babysitters as well as submitter review system to verify exceptional
+            service
+          </p>
+          <p className="flex flex-1 justify-center bg-gray-400 rounded p-6 shadow">
+            Babysitters are sent to houses when they are needed, we guarantee
+            that our certified babysitters are great with kids and moms can take
+            a break without worry
+          </p>
+        </div>
       </div>
     </main>
   );
