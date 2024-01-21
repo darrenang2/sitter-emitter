d882d27eaa9faeb511668cd1a1fdc44cf6ad97de
darrenang2 <darrenang2001@gmail.com>
2024-01-20 17:38:21 -0800

    fixed merge errors

--- src/app/page.js
+++ src/app/page.js
@@ -1,34 +1,34 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-1">
-      <div className="relative">
-        <div className="opacity-50">
-        <Image src="/midori.jpg" alt="Baby pic" width={1500} height={500} />
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div className="relative h-64">
+        <div className="opacity-30 z-10">
+          <Image src="/midori.jpg" alt="Baby pic" width={1500} height={100} />
+        </div>
+        <p className="absolute top-0 left-1/2 transform -translate-x-1/2 text-white p-20 text-7xl font-serif z-20">
+          Sitter Emitter
+        </p>
+      </div>
+      <div className="z-20 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <div className="flex justify-center space-x-4 mx-8">
-          <p className="flex flex-1 justify-center bg-gray-400 rounded p-6 shadow">
+          <p className="flex flex-1 justify-center bg-gray-400 rounded p-5 shadow">
             Thorough background check and certification to ensure well trained
             babysitters as well as submitter review system to verify exceptional
             service
           </p>
-          <p className="flex flex-1 justify-center bg-gray-400 rounded p-6 shadow">
+          <p className="flex flex-1 justify-center bg-gray-400 rounded p-5 shadow">
             Babysitters are sent to houses when they are needed, we guarantee
             that our certified babysitters are great with kids and moms can take
             a break without worry
           </p>
         </div>
-    </div>
-  <p className= "absolute top-0 left-1/2 transform -translate-x-1/2 text-white p-20 text-7xl font-serif">
-    Sitter Emitter
-  </p>
       </div>
-</div>
-      <div className="mb-32 grid text-center lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="mb-32 grid text-center lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left z-20">
         <div className="imgWithDes">
           <a href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app">
-            <button id="emergency">
-                Emergency Babysit
-            </button>
+            <button id="emergency">Emergency Babysit</button>
           </a>
           <p className="des">
             $40 per hour. A licensed, sitter certified professional will come to
@@ -38,9 +38,7 @@ export default function Home() {
 
         <div className="imgWithDes">
           <a href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app">
-            <button id="tier1">
-                Tier 1
-            </button>
+            <button id="tier1">Tier 1</button>
           </a>
           <p className="des">
             $200 per month. Professional sitters will arrive to the location for
@@ -50,9 +48,7 @@ export default function Home() {
 
         <div className="imgWithDes">
           <a href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app">
-            <button id="tier2">
-                Tier 2
-            </button>
+            <button id="tier2">Tier 2</button>
           </a>
           <p className="des">
             $300 per month. Professional sitters will arrive to the location for
@@ -62,9 +58,7 @@ export default function Home() {
 
         <div className="imgWithDes">
           <a href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app">
-            <button id="tier3">
-                Tier 3
-            </button>
+            <button id="tier3">Tier 3</button>
           </a>
           <p className="des">
             $500 per month. Professional sitters will arrive to the location for
